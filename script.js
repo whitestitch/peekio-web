9ed89338d7b75537726a6761d5b343c9951fb00d
whitestitch <48719785+whitestitch@users.noreply.github.com>
2025-07-28 16:19:14 +0200

    Add files via upload
    Initial website upload.

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function ()
+{
+    const mobileMenuButton = document.getElementById('mobile-menu-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    if (mobileMenuButton && mobileMenu)
+    {
+        mobileMenuButton.addEventListener('click', () =>
+        {
+            mobileMenu.classList.toggle('hidden');
+        });
+    }
+});
